aa98fd6aeba44894d45ca8e727b3f3b72dc728c1
Pedro Xavier <307733749+xavier-pedro@users.noreply.github.com>
2026-09-18 16:42:51 -0300

    Faz exercicio 05

--- foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
+++ foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
@@ -1,4 +1,5 @@
 /*
+firstName, lastName, thisYear, ebirthYear
  First name: Carlos
  Last name: Stevenson
  This year: 1965
@@ -8,6 +9,20 @@
 */
 
 // Add your code right below, good luck!
+
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+
+
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear;
+
+const greeting = `Hello! My name is ${fullName} and I am ${age} years old.`;
+
+
+
 
 
 
